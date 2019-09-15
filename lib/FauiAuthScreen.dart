28eee4983cedb0b7f9f7e4fd56cf0382eee8cf9f
Polina Cherkasova <polina.c@live.com>
2019-09-15 15:06:53 -0700

    refactor sizing

--- lib/FauiAuthScreen.dart
+++ lib/FauiAuthScreen.dart
@@ -31,10 +31,10 @@ enum AuthScreen {
 }
 
 class _FauiAuthScreenState extends State<FauiAuthScreen> {
-  static const double _BoxWidth = 500;
-  static const double _BoxHeight = 500;
-  static const double _MinMargin = 20;
-  static const double _Padding = 5;
+  static const double _BoxWidth = 350;
+  static const double _BoxHeight = 380;
+  static const double _MinMargin = 25;
+  static const double _Padding = 25;
 
   AuthScreen _authScreen = AuthScreen.signIn;
   String _error;
@@ -85,18 +85,12 @@ class _FauiAuthScreenState extends State<FauiAuthScreen> {
     ];
   }
 
-  static bool _IsSmall(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    return (screenWidth < _BoxWidth + _MinMargin) ||
-        (screenHeight < _BoxHeight + _MinMargin);
-  }
-
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    bool isSmall = _IsSmall(context);
+    bool isSmall = (screenWidth < _BoxWidth + _MinMargin) ||
+        (screenHeight < _BoxHeight + _MinMargin);
 
     return Container(
       color: Colors.black87,
@@ -109,9 +103,38 @@ class _FauiAuthScreenState extends State<FauiAuthScreen> {
         borderRadius: isSmall
             ? BorderRadius.all(Radius.circular(0))
             : BorderRadius.all(Radius.circular(3)),
-        child: _getScreen(context),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(_getScreenTitle()),
+            actions: this.getActions(),
+          ),
+          body: Center(
+            child: Container(
+              padding: EdgeInsets.only(top: _Padding),
+              width: _BoxWidth - _Padding * 2,
+              child: _getScreen(context),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  String _getScreenTitle() {
+    switch (this._authScreen) {
+      case AuthScreen.signIn:
+        return 'Sign In';
+      case AuthScreen.createAccount:
+        return 'Create Account';
+      case AuthScreen.forgotPassword:
+        return 'Forgot Password';
+      case AuthScreen.verifyEmail:
+        return 'Verify Email';
+      case AuthScreen.resetPassword:
+        return 'Reset Password';
+      default:
+        throw "Unexpected screen $_authScreen";
+    }
   }
 
   Widget _getScreen(BuildContext context) {
@@ -134,63 +157,48 @@ class _FauiAuthScreenState extends State<FauiAuthScreen> {
   Widget _buildCreateAccountScreen(BuildContext context) {
     final TextEditingController emailController =
         new TextEditingController(text: this._email);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Account'),
-        actions: this.getActions(),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: _Padding),
-          width: _BoxWidth - _Padding * 2,
-          child: Column(
-            children: <Widget>[
-              TextField(
-                autofocus: true,
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "EMail",
-                ),
-              ),
-              Text(
-                this._error ?? "",
-                style: TextStyle(color: Colors.red),
-              ),
-              RaisedButton(
-                child: Text('Create Account'),
-                onPressed: () async {
-                  try {
-                    await FbConnector.RegisterUser(
-                      apiKey: this.widget.firebaseApiKey,
-                      email: emailController.text,
-                    );
-
-                    await FbConnector.SendResetLink(
-                      apiKey: this.widget.firebaseApiKey,
-                      email: emailController.text,
-                    );
-
-                    this.switchScreen(
-                        AuthScreen.verifyEmail, emailController.text);
-                  } catch (e) {
-                    this.setState(() {
-                      this._error = FauiExceptionAnalyser.ToUiMessage(e);
-                      this._email = emailController.text;
-                    });
-                  }
-                },
-              ),
-              FlatButton(
-                child: Text('Have account? Sign in.'),
-                onPressed: () {
-                  this.switchScreen(AuthScreen.signIn, emailController.text);
-                },
-              ),
-            ],
-          ),
+    return Column(children: <Widget>[
+      TextField(
+        autofocus: true,
+        controller: emailController,
+        decoration: InputDecoration(
+          labelText: "EMail",
         ),
       ),
-    );
+      Text(
+        this._error ?? "",
+        style: TextStyle(color: Colors.red),
+      ),
+      RaisedButton(
+        child: Text('Create Account'),
+        onPressed: () async {
+          try {
+            await FbConnector.RegisterUser(
+              apiKey: this.widget.firebaseApiKey,
+              email: emailController.text,
+            );
+
+            await FbConnector.SendResetLink(
+              apiKey: this.widget.firebaseApiKey,
+              email: emailController.text,
+            );
+
+            this.switchScreen(AuthScreen.verifyEmail, emailController.text);
+          } catch (e) {
+            this.setState(() {
+              this._error = FauiExceptionAnalyser.ToUiMessage(e);
+              this._email = emailController.text;
+            });
+          }
+        },
+      ),
+      FlatButton(
+        child: Text('Have account? Sign in.'),
+        onPressed: () {
+          this.switchScreen(AuthScreen.signIn, emailController.text);
+        },
+      ),
+    ]);
   }
 
   Widget _buildSignInScreen(BuildContext context) {
@@ -206,139 +214,100 @@ class _FauiAuthScreenState extends State<FauiAuthScreen> {
     //   }
     // });
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Sign In',
+    return Column(
+      children: <Widget>[
+        RawKeyboardListener(
+          child: TextField(
+            autofocus: true,
+            controller: emailController,
+            decoration: InputDecoration(
+              labelText: "EMail",
+            ),
+          ),
+          onKey: (dynamic key) {
+            if (key.data.keyCode == 9) {
+              FocusScope.of(context).requestFocus(_passwordFocus);
+            }
+          },
+          focusNode: _emailFocus,
         ),
-        actions: this.getActions(),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 50.0),
-          width: 400.0,
-          child: Column(
-            children: <Widget>[
-              RawKeyboardListener(
-                child: TextField(
-                  autofocus: true,
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "EMail",
-                  ),
-                ),
-                onKey: (dynamic key) {
-                  if (key.data.keyCode == 9) {
-                    FocusScope.of(context).requestFocus(_passwordFocus);
-                  }
-                },
-                focusNode: _emailFocus,
-              ),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                focusNode: _passwordFocus,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                ),
-              ),
-              Text(
-                this._error ?? "",
-                style: TextStyle(color: Colors.red),
-              ),
-              RaisedButton(
-                child: Text('Sign In'),
-                onPressed: () async {
-                  try {
-                    FauiUser user = await FbConnector.SignInUser(
-                      apiKey: this.widget.firebaseApiKey,
-                      email: emailController.text,
-                      password: passwordController.text,
-                    );
-                    this.afterAuthorized(context, user);
-                  } catch (e) {
-                    this.setState(() {
-                      this._error = FauiExceptionAnalyser.ToUiMessage(e);
-                      this._email = emailController.text;
-                    });
-                  }
-                },
-              ),
-              FlatButton(
-                child: Text('Create Account'),
-                onPressed: () {
-                  this.switchScreen(
-                      AuthScreen.createAccount, emailController.text);
-                },
-              ),
-              FlatButton(
-                child: Text('Forgot Password?'),
-                onPressed: () {
-                  this.switchScreen(
-                      AuthScreen.forgotPassword, emailController.text);
-                },
-              ),
-            ],
+        TextField(
+          controller: passwordController,
+          obscureText: true,
+          focusNode: _passwordFocus,
+          decoration: InputDecoration(
+            labelText: "Password",
           ),
         ),
-      ),
+        Text(
+          this._error ?? "",
+          style: TextStyle(color: Colors.red),
+        ),
+        RaisedButton(
+          child: Text('Sign In'),
+          onPressed: () async {
+            try {
+              FauiUser user = await FbConnector.SignInUser(
+                apiKey: this.widget.firebaseApiKey,
+                email: emailController.text,
+                password: passwordController.text,
+              );
+              this.afterAuthorized(context, user);
+            } catch (e) {
+              this.setState(() {
+                this._error = FauiExceptionAnalyser.ToUiMessage(e);
+                this._email = emailController.text;
+              });
+            }
+          },
+        ),
+        FlatButton(
+          child: Text('Create Account'),
+          onPressed: () {
+            this.switchScreen(AuthScreen.createAccount, emailController.text);
+          },
+        ),
+        FlatButton(
+          child: Text('Forgot Password?'),
+          onPressed: () {
+            this.switchScreen(AuthScreen.forgotPassword, emailController.text);
+          },
+        ),
+      ],
     );
   }
 
   Widget _buildVerifyEmailScreen(BuildContext context, String email) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Verify Email'),
-        actions: this.getActions(),
-      ),
-      body: Center(
-        child: Container(
-          width: 400.0,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 50.0),
-                child: Text("We sent verification link to $email"),
-              ),
-              RaisedButton(
-                child: Text('Sign In'),
-                onPressed: () {
-                  this.switchScreen(AuthScreen.signIn, email);
-                },
-              ),
-            ],
-          ),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 50.0),
+          child: Text("We sent verification link to $email"),
         ),
-      ),
+        RaisedButton(
+          child: Text('Sign In'),
+          onPressed: () {
+            this.switchScreen(AuthScreen.signIn, email);
+          },
+        ),
+      ],
     );
   }
 
   Widget _buildResetPasswordScreen(BuildContext context, String email) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Reset Password'),
-        actions: this.getActions(),
-      ),
-      body: Center(
-        child: Container(
-          width: 400.0,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 50.0),
-                child:
-                    Text("We sent the link to reset your password to $email"),
-              ),
-              RaisedButton(
-                child: Text('Sign In'),
-                onPressed: () {
-                  this.switchScreen(AuthScreen.signIn, email);
-                },
-              ),
-            ],
-          ),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 50.0),
+          child: Text("We sent the link to reset your password to $email"),
         ),
-      ),
+        RaisedButton(
+          child: Text('Sign In'),
+          onPressed: () {
+            this.switchScreen(AuthScreen.signIn, email);
+          },
+        ),
+      ],
     );
   }
 
@@ -346,62 +315,49 @@ class _FauiAuthScreenState extends State<FauiAuthScreen> {
     final TextEditingController emailController =
         new TextEditingController(text: this._email);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Reset Password'),
-        actions: this.getActions(),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 50.0),
-          width: 400.0,
-          child: Column(
-            children: <Widget>[
-              TextField(
-                autofocus: true,
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "EMail",
-                ),
-              ),
-              Text(
-                this._error ?? "",
-                style: TextStyle(color: Colors.red),
-              ),
-              RaisedButton(
-                child: Text('Send Password Reset Link'),
-                onPressed: () async {
-                  try {
-                    await FbConnector.SendResetLink(
-                      apiKey: this.widget.firebaseApiKey,
-                      email: emailController.text,
-                    );
-                    this.switchScreen(
-                        AuthScreen.resetPassword, emailController.text);
-                  } catch (e) {
-                    this.setState(() {
-                      this._error = FauiExceptionAnalyser.ToUiMessage(e);
-                      this._email = emailController.text;
-                    });
-                  }
-                },
-              ),
-              FlatButton(
-                child: Text('Sign In'),
-                onPressed: () {
-                  this.switchScreen(AuthScreen.signIn, email);
-                },
-              ),
-              FlatButton(
-                child: Text('Create Account'),
-                onPressed: () {
-                  this.switchScreen(AuthScreen.createAccount, email);
-                },
-              ),
-            ],
+    return Column(
+      children: <Widget>[
+        TextField(
+          autofocus: true,
+          controller: emailController,
+          decoration: InputDecoration(
+            labelText: "EMail",
           ),
         ),
-      ),
+        Text(
+          this._error ?? "",
+          style: TextStyle(color: Colors.red),
+        ),
+        RaisedButton(
+          child: Text('Send Password Reset Link'),
+          onPressed: () async {
+            try {
+              await FbConnector.SendResetLink(
+                apiKey: this.widget.firebaseApiKey,
+                email: emailController.text,
+              );
+              this.switchScreen(AuthScreen.resetPassword, emailController.text);
+            } catch (e) {
+              this.setState(() {
+                this._error = FauiExceptionAnalyser.ToUiMessage(e);
+                this._email = emailController.text;
+              });
+            }
+          },
+        ),
+        FlatButton(
+          child: Text('Sign In'),
+          onPressed: () {
+            this.switchScreen(AuthScreen.signIn, email);
+          },
+        ),
+        FlatButton(
+          child: Text('Create Account'),
+          onPressed: () {
+            this.switchScreen(AuthScreen.createAccount, email);
+          },
+        ),
+      ],
     );
   }
 }
