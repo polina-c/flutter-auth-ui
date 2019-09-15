32708622c0f21d66cf52e6b10dd413c1c8b1cc65
Polina Cherkasova <polina.c@live.com>
2019-09-15 13:32:24 -0700

    silent sign on

--- lib/FauiLocalStorage.dart
+++ lib/FauiLocalStorage.dart
@@ -5,55 +5,76 @@ import 'faui_model.dart';
 
 import 'FbConnector.dart';
 
+import 'package:crypted_preferences/crypted_preferences.dart';
+
 class FauiLocalStorage {
+  static Preferences _prefs;
+  static const String _LocalKey = "user";
+
+  static void _ThowIfNotInitialized() {
+    if (_prefs == null)
+      throw "To use local storage call faui.TrySignInSilently() before app start in order to initialize local storage.";
+  }
+
+  static Future _Initialize() async {
+    try {
+      _prefs = await Preferences.preferences(path: 'pathToPrefs');
+    } catch (ex) {
+      print('error initializing:');
+      print(ex);
+    }
+  }
+
   static void SaveUserLocallyForSilentSignIn() {
     _StoreLocally(_LocalKey, jsonEncode(FauiAuthState.User));
-    print("ssi: saved locally");
+    print("sso: saved locally");
   }
 
   static void DeleteUserLocally() {
     _DeleteLocally(_LocalKey);
-    print("ssi: deleted locally");
+    print("sso: deleted locally");
   }
 
   static Future TrySignInSilently(String apiKey) async {
-    print("ssi: started silent sign-in");
+    print("sso: started silent sign-in");
     try {
+      await _Initialize();
       FauiUtil.ThrowIfNullOrEmpty(value: apiKey, name: "apiKey");
       String v = _GetLocalValue(_LocalKey);
       if (v == null || v == "null") {
-        print("ssi: no user stored");
+        print("sso: no user stored");
         return;
       }
 
       FauiUser user = FauiUser.fromJson(jsonDecode(v));
       if (user == null || user.refreshToken == null) {
-        print("ssi: no refresh token found");
+        print("sso: no refresh token found");
         return;
       }
       user = await FbConnector.RefreshToken(user: user, apiKey: apiKey);
       _StoreLocally(_LocalKey, jsonEncode(user));
       FauiAuthState.User = user;
-      print("ssi: succeeded silent sign-in");
+      print("sso: succeeded silent sign-in");
       return;
     } catch (ex) {
-      print("ssi: error during silent sign-in:");
+      print("sso: error during silent sign-in:");
       print(ex.toString());
       return;
     }
   }
 
-  static const _LocalKey = "user";
-
   static void _DeleteLocally(String key) {
-    //window.localStorage.remove(key);
+    _ThowIfNotInitialized();
+    _prefs.remove(key);
   }
 
   static String _GetLocalValue(String key) {
-    //return window.localStorage[key];
+    _ThowIfNotInitialized();
+    return _prefs.getString(key);
   }
 
   static void _StoreLocally(String key, String value) {
-    //window.localStorage[key] = value;
+    _ThowIfNotInitialized();
+    _prefs.setString(key, value);
   }
 }
