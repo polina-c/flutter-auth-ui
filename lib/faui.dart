725f40a635719f78736b7c7adb97ccd20e8bcb75
Polina Cherkasova <polina.c@live.com>
2019-10-16 22:01:35 -0700

    documented API

--- lib/faui.dart
+++ lib/faui.dart
@@ -4,30 +4,39 @@ import 'package:faui/src/FauiLocalStorage.dart';
 import 'package:faui/src/FauiUser.dart';
 import 'package:flutter/material.dart';
 
+///Facade class for the library 'faui'
 class Faui {
+  /// Returns the signed-in user or null
   static FauiUser get user {
     return FauiAuthState.user;
   }
 
-  static set user(v) {
+  /// Sets currently signed-in user
+  static set user(FauiUser v) {
     FauiAuthState.user = v;
   }
 
+  /// Signes out the user
   static void signOut() {
     FauiAuthState.user = null;
     FauiLocalStorage.deleteUserLocally();
   }
 
+  /// Saves user locally to enable silent sign-in
   static void saveUserLocallyForSilentSignIn() {
     FauiLocalStorage.saveUserLocallyForSilentSignIn();
   }
 
+  /// Tries to sign-in silently
   static Future trySignInSilently({
     @required String firebaseApiKey,
   }) async {
     return await FauiLocalStorage.trySignInSilently(firebaseApiKey);
   }
 
+  /// Builds sign-in dialog. If startWithRegistration is true, the dialog
+  /// first suggests user to create account with option to sign-in, otherwize
+  /// it suggests user to sign-in with option to create account.
   static Widget buildAuthScreen(
       {@required VoidCallback onExit,
       @required String firebaseApiKey,
